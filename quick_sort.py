c770fae38ceae37b0f5c706d679f36717bd28bf7
Vasilis Mouchakis <mouchakisvasilis@gmail.com>
2021-03-06 18:06:01 +0200

    a simple example of the quick sort algorithm

---
+++ quick_sort.py
@@ -0,0 +1,20 @@
+def quick_sort(sequence):
+    length = len(sequence)          # we use a list as a sequence 
+    if length <= 1:
+        return sequence
+    else:
+        pivot = sequence.pop()      # we take as pivot the last character of the list
+
+    items_lower = []
+    items_greater = []
+
+    for item in sequence:
+        if item > pivot:
+            items_greater.append(item)
+        else:
+            items_lower.append(item)
+
+    return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
+
+
+print(quick_sort([3, 5, 1, 7, 4, 8]))
